6388df75882e2859e759cf1d84611246b1151061
Enri Zhulati <mbp-ez@Mac.attlocal.net>
2025-03-19 01:24:18 -0500

    Fix: Add key prop to BlogPost component to ensure proper re-mounting when navigating between posts

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,10 +1,13 @@
 import BlogPostComponent from '../components/blog/BlogPost';
 import PageTransition from '../components/PageTransition';
+import { useParams } from 'react-router-dom';
 
 const BlogPostPage = () => {
+  const { slug } = useParams<{ slug: string }>();
+  
   return (
     <PageTransition>
-      <BlogPostComponent />
+      <BlogPostComponent key={slug} />
     </PageTransition>
   );
 };
